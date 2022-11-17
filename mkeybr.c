a0d3589090b4b4adaac0e2ed3db7c55eace5c652
Davide Bresolin <bresolin.davide@gmail.com>
2022-11-17 20:40:06 +0100

    Better check for COMBIs and CTRL+Key

--- mkeybr.c
+++ mkeybr.c
@@ -307,24 +307,8 @@ int cdecl NAME(cint15_handler)(uchar scancode)
 				return scancode;
 		if(keyflags & _KCHAR)
 			tbl += 2;
-#ifdef COMBI
-		/* Duplicate simulateKeyPress code to avoid COMBIs clash with ^A */
-		if (tbl[0] == IGNORE)
-			return scancode;	/* let BIOS do its work */
 
-								/* strange, but necessary */
-		if (tbl[0] == 0xf0 ||   /* this is a BIOS 'feature' */
-			tbl[0] == 0xe0)     /* this too               */
-		{
-			scancode = 0;
-		}
-
-		GENERATE_KEYSTROKE(scancode,tbl[0]);
-		
-		return 0;
-#else
-		goto simulateKeyPress;
-#endif
+		goto simulateCtrlKeyPress;		/* Skip check for COMBIs to avoid clash with ^A..^F */
 	}
 
 	if ((keyflags & _KCHAR)  == 0)  /* it should have _KCHAR defined */
@@ -345,16 +329,19 @@ int cdecl NAME(cint15_handler)(uchar scancode)
 
 simulateKeyPress:
 
-	if (tbl[0] == IGNORE)
-		return scancode;	/* let BIOS do its work */
-
 #ifdef COMBI
-	if (tbl[0] <= COMBI6)
+	if (tbl[0] >= COMBI1 && tbl[0] <= COMBI6)
 	{
 		currentCombi         = tbl[0];
 		return 0;
 	}
 #endif
+
+simulateCtrlKeyPress:
+
+	if (tbl[0] == IGNORE)
+		return scancode;	/* let BIOS do its work */
+
 							/* strange, but necessary */
 	if (tbl[0] == 0xf0 ||   /* this is a BIOS 'feature' */
 		tbl[0] == 0xe0)     /* this too               */

7fcc815f2da1d488a2a764b114b86af3fb997e4c
Davide Bresolin <bresolin.davide@gmail.com>
2023-07-08 22:31:54 +0200

    Fix keypad . in UK keymap

--- keydefuk.h
+++ keydefuk.h
@@ -50,6 +50,6 @@ struct KeyboardDefinition NAME(Keyboard) = {
 #else
         { 0 },
 #endif                  
-        ',',                                           //char DezimalDingsbums;
+        0,                                             //char DezimalDingsbums;
         0,                                             //char DefaultLayoutUS;                  // TRUE for russian
         } ;

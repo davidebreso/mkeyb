b650117030f8d047bfa2b061643a469b7ee5dd67
Turo Lamminen <turol@iki.fi>
2026-05-02 19:54:29 +0300

    Fix scancode table validation failure on GCC-ia16

--- mkeyb.c
+++ mkeyb.c
@@ -167,7 +167,12 @@ void VerifyScancodeTableForCorrectness(	struct KeyboardDefinition *kb,int print)
 		lasttbl = tbl;
 	}
 
-	if (tbl + 1 != scancode_end)
+	if (tbl + 1 != scancode_end
+#ifdef __GNUC__
+		/* GCC-ia16 aligns symbols to 2 bytes which sometimes adds an extra byte */
+		&& tbl + 2 != scancode_end
+#endif  /* __GNUC__ */
+	   )
 	{
 		printf("scancode table error: early zero found at offset 0x%2x last scan 0x%02x\n",
 				tbl-kb->ScancodeTable, lasttbl[0]);
